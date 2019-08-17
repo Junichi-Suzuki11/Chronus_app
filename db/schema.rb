593bf045969b382b89172f145272fd50f54b4acb
Junichi-Suzuki11 <largojs@gmail.com>
2019-08-17 07:58:44 +0000

    [Add]usersテーブルにカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2019_08_17_072133) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
+    t.string "name"
+    t.string "account_name"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
